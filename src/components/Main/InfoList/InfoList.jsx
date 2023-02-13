37e9a7997deb50c302fd61f50635bb7fab3b2fb4
Guille-Rubio <guillermorubiog@gmail.com>
2023-02-13 15:54:12 +0100

    ADD print, add, removeall, remove one, restore cards

--- src/components/Main/InfoList/InfoList.jsx
+++ src/components/Main/InfoList/InfoList.jsx
@@ -1,17 +1,62 @@
 import React, { Component } from "react";
-import InfoItem from './InfoItem'
+import InfoItem from './InfoItem';
+import data from './events.json';
+import { v4 as uuidv4 } from 'uuid';
+
 class InfoList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      events: data
+    }
+  }
+
+  deleteEvent = (i) => {
+    const remainingEvents = this.state.events.filter((event, j) => i !== j);
+    this.setState({ events: remainingEvents })
+  }
+
+
+  printCards = () => this.state.events.map((event, i) => <InfoItem data={event} remove={() => this.deleteEvent(i)} key={uuidv4()} />)
+
+  addEvent = () => {
+    const promptEventName = prompt('Nombre del evento?');
+    const promptInfo = prompt('Cuales el la info del evento?');
+    const promptPrice = prompt('Cual es el precio del evento?');
+    const promptImage = prompt('Añade la imagen del evento');
+
+    const newEvent = {
+      eventName: promptEventName,
+      info: promptInfo,
+      price: promptPrice,
+      image: promptImage
+    };
+
+    this.setState({ events: [...this.state.events, newEvent] });
+  }
+
+  removeAllEvents = () => {
+    this.setState({ events: [] })
+  }
+
+  restoreEvents = () => {
+    this.setState({ events: data })
+  }
+
+
+
+
+
   render() {
     return <section>
       <h2>Info de eventos</h2>
-      <InfoItem/>
-      <InfoItem/>
-      <InfoItem/>
-      <InfoItem/>
-      <InfoItem text="Vamos a por tortilla" url="https://cdn.pixabay.com/photo/2016/03/27/17/14/venetian-mask-1283163_960_720.jpg"/>
-      <InfoItem text="Alguien se va a pedir barrita tomate" url="https://cdn.pixabay.com/photo/2018/01/17/15/33/streamer-3088458_960_720.jpg"/>
-      <InfoItem text="Zumo de naranaja es bueno" url="https://cdn.pixabay.com/photo/2013/07/27/05/09/carousel-168125_960_720.jpg"/>
-      <InfoItem text="Un café con leche, por favor" url="https://cdn.pixabay.com/photo/2017/02/23/18/00/carnival-2092819_960_720.jpg" />
+      <button onClick={this.addEvent}>Add Event</button>
+      <button onClick={this.removeAllEvents}>Remove All Events</button>
+      <button onClick={this.restoreEvents}>Restore Events</button>
+
+      {this.printCards()}
+
     </section>;
   }
 }
