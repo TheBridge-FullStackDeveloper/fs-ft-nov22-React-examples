b2c45060a1debeec0c27a09fffee020a890d29ff
Alejandro Reyes <reyesbalejandroe@gmail.com>
2023-02-14 12:23:35 +0100

    add/event_handling_ref_form

--- src/components/Main/InfoList/InfoList.jsx
+++ src/components/Main/InfoList/InfoList.jsx
@@ -8,8 +8,11 @@ class InfoList extends Component {
     super(props)
 
     this.state = {
-      events: data
+      events: data, // [] de datos iniales
+      newEventName: "", // Nombre del evento que estoy creando
+      userName: ""
     }
+    this.userName = React.createRef(); // Referencia para nombre usuario
   }
 
   deleteEvent = (i) => {
@@ -18,7 +21,7 @@ class InfoList extends Component {
   }
 
 
-  printCards = () => this.state.events.map((event, i) => <InfoItem data={event} remove={() => this.deleteEvent(i)} key={uuidv4()} />)
+  printCards = () => <section>{this.state.events.map((event, i) => <InfoItem data={event} remove={() => this.deleteEvent(i)} key={uuidv4()} />)}</section>
 
   addEvent = () => {
     const promptEventName = prompt('Nombre del evento?');
@@ -45,17 +48,90 @@ class InfoList extends Component {
   }
 
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target.name.value);
 
+    const eventName = event.target.name.value;
+    const info = event.target.info.value;
+    const price = event.target.price.value;
+    const image = event.target.image.value;
+
+    const newEvent = {
+      eventName,
+      info,
+      price,
+      image
+    };
+
+    console.log(newEvent);
+    alert("Enviado!!!!!!");
+    // Guardar nuevo evento
+    // (En el futuro puede ser un POST a una API)
+    this.setState({ events: [...this.state.events, newEvent] });
+  }
+
+  handleChange = (event) => this.setState({ newEventName: event.target.value })
+
+  handleClick = () => {
+    alert("Nombre de usuario: " + this.userName.current.value); // Leer referencia
+    this.setState({ userName: this.userName.current.value }); // Setear estado userName
+  }
+
+  removeName = () => {
+    alert("borrado!")
+    this.setState({ userName: "" }); // Borrar userName
+  }
+
+  manageUser = () => {
+    if (this.state.userName) {
+      return <article>
+        <h2>Welcome, {this.state.userName}</h2>
+        <button onClick={this.removeName}>Salir</button>
+      </article>
+    } else {
+      return <article>
+        <h2>Introduce tu nombre</h2>
+        <input type="text" ref={this.userName} />
+        <button onClick={this.handleClick}>Enviar</button>
+      </article>
+    }
+  }
 
 
   render() {
     return <section>
-      <h2>Info de eventos</h2>
-      <button onClick={this.addEvent}>Add Event</button>
-      <button onClick={this.removeAllEvents}>Remove All Events</button>
-      <button onClick={this.restoreEvents}>Restore Events</button>
 
-      {this.printCards()}
+      {this.manageUser()}
+
+      <article>
+        <h2>Añadir eventos</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Nombre:</label><br />
+          <input type="text" id="name" name="name" onChange={this.handleChange} /><br />
+          <label htmlFor="info">Info:</label><br />
+          <input type="text" id="info" name="info" /><br />
+          <label htmlFor="price">Precio:</label><br />
+          <input type="number" id="price" name="price" /><br />
+          <label htmlFor="image">URL imágen:</label><br />
+          <input type="url" id="image" name="image" /><br />
+          <input type="submit" value="Añadir" />
+        </form>
+        {this.state.newEventName ? <p>Nuevo evento a crear: {this.state.newEventName}</p> : ""}
+
+      </article>
+
+      <article>
+        <h2>Opciones</h2>
+        <button onClick={this.addEvent}>Add Event</button>
+        <button onClick={this.removeAllEvents}>Remove All Events</button>
+        <button onClick={this.restoreEvents}>Restore Events</button>
+      </article>
+
+      <article>
+        <h2>Nuestros eventos</h2>
+        {this.printCards()}
+      </article>
 
     </section>;
   }
