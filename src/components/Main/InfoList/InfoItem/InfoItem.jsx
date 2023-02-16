6549ec3a7383ace7aeb18d807f9ecd924b8b6215
Guille-Rubio <guillermorubiog@gmail.com>
2023-02-16 17:52:14 +0100

    ADD Sass styles and Material UI

--- src/components/Main/InfoList/InfoItem/InfoItem.jsx
+++ src/components/Main/InfoList/InfoItem/InfoItem.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
-import './InfoItem.css'
+import './InfoItem.css';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 class InfoItem extends Component {
   //rconst
@@ -7,19 +13,45 @@ class InfoItem extends Component {
     super(props)
 
     this.state = {
-      event:this.props.data 
+      event: this.props.data
     }
   }
 
   render() {
-    return (<article>
-      <h1>Event</h1>
-      <p>{this.props.data.eventName}</p>
-      <img src={this.props.data.image} alt="imagen de evento" className="img_event"/>
-      <p>{this.props.data.info}</p>
-      <p>{this.props.data.price}</p>
-      <button onClick={this.props.remove}>Delete</button>
-      </article>
+    return (
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image={this.props.data.image}
+          title="event picture"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {this.props.data.eventName}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {this.props.data.info}
+          </Typography>
+          <Typography>
+            <p>{this.props.data.info}</p>
+          </Typography>
+          <Typography>
+            <p>{this.props.data.price}</p>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      /*  <article>
+         <h1>Event</h1>
+         <p>{this.props.data.eventName}</p>
+         <img src={this.props.data.image} alt="imagen de evento" className="img_event"/>
+         <p>{this.props.data.info}</p>
+         <p>{this.props.data.price}</p>
+         <button onClick={this.props.remove}>Delete</button>
+         </article> */
     )
   }
 }
