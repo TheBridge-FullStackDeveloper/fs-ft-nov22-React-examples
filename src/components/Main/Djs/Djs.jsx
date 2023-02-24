a2fcda9613a63daad9362943e3609bdf56415efd
Alejandro Reyes <reyesbalejandroe@gmail.com>
2023-02-24 11:01:13 +0100

    add/spinner

--- src/components/Main/Djs/Djs.jsx
+++ src/components/Main/Djs/Djs.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import useFetch from '../../../hooks/useFetch';
 import { v4 as uuidv4 } from 'uuid';
+import { InfinitySpin } from 'react-loader-spinner'
+import useFetch from '../../../hooks/useFetch';
 
 function Djs() {
   // const [pokemons, setPokemons] = useState([]);
 
 
-  const {loading,result} = useFetch('https://pokeapi.co/api/v2/pokemon');
+  const { loading, result } = useFetch('https://pokeapi.co/api/v2/pokemon');
   console.log(result.results);
 
 
@@ -23,7 +24,18 @@ function Djs() {
   return (
     <section>
       <h1>DJs residentes</h1>
-      {loading?"carganding....":result.results.map((pokemon) => <h3 key={uuidv4()}>{pokemon.name}</h3>)}
+
+
+
+      {loading ? <>
+
+        <InfinitySpin
+          width='200'
+          color="#000"
+        />
+        <p>carganding....</p>
+
+      </> : result.results.map((pokemon) => <h3 key={uuidv4()}>{pokemon.name}</h3>)}
 
     </section>
 
